ba5708f70a6f78b61791541cbedc7815a2f49808
Braeden Smith <braedensmith629@gmail.com>
2026-03-12 11:31:21 -0500

    expand-functionality: Improved background handling of icons

--- src/background/background.ts
+++ src/background/background.ts
@@ -7,31 +7,51 @@ browser.runtime.onStartup.addListener(async (): Promise<void> => {
     const pause = await Pause.create();
     if (pause.getPauseStatus() === 'session') {
         await pause.unpause();
-    } else if (pause.isPaused()) {
-        await browser.browserAction.setIcon({
-            path: {
-                32: '../icons/paused/icon32.png',
-                64: '../icons/paused/icon64.png',
-            }
-        });
     }
 });
 
-browser.runtime.onInstalled.addListener(async (details): Promise<void> => {
-    const pause = await Pause.create();
-    if (pause.isPaused()) {
-        await browser.browserAction.setIcon({
+function setProperIcon(pause: Pause): void {
+    const pauseStatus = pause.getPauseStatus();
+    if (pauseStatus === null) {
+        void browser.browserAction.setIcon({
+            path: {
+                32: '../icons/enabled/icon32.png',
+                64: '../icons/enabled/icon64.png',
+            }
+        });
+    } else {
+        void browser.browserAction.setIcon({
             path: {
                 32: '../icons/paused/icon32.png',
                 64: '../icons/paused/icon64.png',
             }
         });
     }
-});
+}
+
+function checkForPauseExpiration(pause: Pause, pausedUntil: number | null): void {
+    const delta = pausedUntil !== null ? pausedUntil - Date.now() : 0;
+    if (delta > 0) {
+        setTimeout(async () => {
+            if (!pause.isPaused()) {
+                await browser.browserAction.setIcon({
+                    path: {
+                        32: '../icons/enabled/icon32.png',
+                        64: '../icons/enabled/icon64.png',
+                    }
+                });
+            }
+        }, delta);
+    }
+}
 
 async function init(): Promise<void> {
     const settings = await Settings.create();
     const pause = await Pause.create();
+
+    setProperIcon(pause);
+    checkForPauseExpiration(pause, pause.getPausedUntil());
+    pause.addOnChangeListener((pauseData) => checkForPauseExpiration(pause, pauseData.pausedUntil));
     
     await TabTracker.create(settings, pause);
     new CommandHandler(settings, pause);
